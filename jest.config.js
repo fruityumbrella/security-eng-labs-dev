9dd8797468771c6ae33304d0bdba91d096dc5a8b
devbishowp <devops@thebishop.ro>
2026-04-24 23:41:02 +0300

    Fix: use local Jest and explicit ts-jest transform

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/apps', '<rootDir>/packages'],
   testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
   collectCoverageFrom: [
     'apps/*/src/**/*.ts',
     'packages/*/src/**/*.ts',
